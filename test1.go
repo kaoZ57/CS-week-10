90dff1315a0de9a504bb97ff3763e7cd26f9ce80
kaoZ57 <57824756+kaoZ57@users.noreply.github.com>
2020-01-25 18:50:10 +0700

    Create test1.go

---
+++ test1.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
+	"time"
+)
+
+func getDrives() (r []string) {
+	for _, drive := range "ABCDEFGHIJKLMNOPQRSTUVWXYZ" {
+		f, err := os.Open(string(drive) + ":\\")
+		if err == nil {
+			d := string(drive) + ":/"
+			r = append(r, d)
+			f.Close()
+		}
+	}
+	return
+}
