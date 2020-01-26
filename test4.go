66e97e00e64a0e56b5140bb52307006bff1c3e04
kaoZ57 <57824756+kaoZ57@users.noreply.github.com>
2020-01-26 18:31:24 +0700

    Update test4.go

--- test4.go
+++ test4.go
@@ -11,3 +11,12 @@ func sayjj(txt string, sleep time.Duration, wg *sync.WaitGroup) {
 	fmt.Println(txt)
 	time.Sleep(time.Microsecond * sleep)
 }
+
+func main() {
+	var wg sync.WaitGroup
+	wg.Add(2)
+	go sayjj("hello", 2, &wg)
+	go sayjj("Hi", 1, &wg)
+	wg.Wait()
+	fmt.Println("GoodBye")
+}
